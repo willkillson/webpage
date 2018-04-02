6ac148200a442c4cec9748c254ccbf0a84fef59a
willkillson <willkillson@gmail.com>
2018-04-02 16:54:58 -0700

    Split my circle notes into a seperate .js, project is now going to be a tetris clone in javascript

---
+++ mywebpage/bin/Debug/AppX/js/circle.js
@@ -0,0 +1,122 @@
+﻿
+
+var mouse = {
+    x: undefined, y: undefined
+}
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    //console.log(mouse.x + " " + mouse.y);
+});
+
+
+var distance = function (x1, y1, x2, y2) {
+
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    dx = dx * dx;
+    dy = dy * dy;
+    return Math.sqrt(dx + dy);
+}
+
+//Circle constructor
+function Circle(x, y, dx, dy, radius, r, g, b) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = 1;
+    this.ograd = radius;
+    this.radius = radius;
+    this.draw = function () {
+
+        c.beginPath();
+
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        c.strokeStyle = `rgba(${this.r},${this.g},${this.b},1)`;
+        c.fill();
+        c.stroke();
+    }
+    this.update = function () {
+        //resize code
+
+        if (distance(this.x, this.y, mouse.x, mouse.y) < this.radius + 40) {
+            this.radius = this.radius + 5;
+        }
+        else if (this.ograd < this.radius + 40) {
+            this.radius = this.radius - 30;
+        }
+
+
+        if ((this.radius < 0) || (this.radius > 1000)) {//reseting big ass bounds
+            this.radius = this.ograd;
+        }
+        //resize code
+
+        this.x = this.x + this.dx;
+        this.y = this.y + this.dy;
+
+        if (this.x >= canvas.width - this.radius) {
+            this.dx = -this.dx;
+        }
+        else if (this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+
+        if (this.y >= canvas.height - this.radius) {
+            this.dy = -this.dy;
+        }
+        else if (this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+
+
+
+
+
+
+    }
+
+}
+
+//array of circles
+var circles = [];
+for (var i = 0; i < 300; i++) {
+
+    let radius = 60 + 45 * Math.random();
+
+    //from 30 to innerwidth - 30
+    let x = radius + (innerWidth - 3 * radius) * Math.random();
+    let y = radius + (innerHeight - 3 * radius) * Math.random();
+
+
+    let dx = 4 * (Math.random() - 0.5);
+    let dy = 4 * (Math.random() - 0.5);
+    let r = Math.floor(256 * Math.random());
+    let g = Math.floor(256 * Math.random());
+    let b = Math.floor(256 * Math.random());
+
+
+    var circle = new Circle(x, y, dx, dy, radius, r, g, b);
+
+    circles.push(circle);
+}
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].draw();
+        circles[i].update();
+    }
+
+}
+
+animate();
--- mywebpage/bin/Debug/AppX/js/main.js
+++ mywebpage/bin/Debug/AppX/js/main.js
@@ -8,21 +8,6 @@ canvas.height = window.innerHeight - 2;
 var c = canvas.getContext('2d');
 
 
-//create an 8x8 grid
-
-//////////////////////////////////////////rectangle
-//c.fillStyle = 'rgba(255,0,0,1)';
-//var GridSize = 40;
-//var fillamount = 1.0;
-//for (var i = 0; i < 8; i++) {
-//    for (var j = 0; j < 8; j++) {
-//        c.fillStyle = `rgba(255,0,0,${fillamount})`;
-//        c.fillRect(GridSize * i+i, GridSize*j +j, GridSize, GridSize);
-//    }
-//    fillamount = (fillamount - 2*(1 / 16));
-//}
-
-
 //////////////////////////////////////////line drawing
 //line
 //c.beginPath();
@@ -62,6 +47,99 @@ var c = canvas.getContext('2d');
 //    c.stroke();
 //}
 
+function Board() {
+ //////////////////////////////////////////rectangle
+//c.fillStyle = 'rgba(255,0,0,1)';
+//var GridSize = 40;
+//var fillamount = 1.0;
+//for (var i = 0; i < 8; i++) {
+//    for (var j = 0; j < 8; j++) {
+//        c.fillStyle = `rgba(255,0,0,${fillamount})`;
+//        c.fillRect(GridSize * i + i, GridSize * j + j, GridSize, GridSize);
+//    }
+//    fillamount = (fillamount - 2 * (1 / 16));
+//}
+    this._gridHeight = 25;
+    this._gridWidth = 11;
+    this._cellSize = 30;
+    this._cellPadding = 5;
+    this.isEmpty = true;//this will invoke a place piece function
+
+    this._board = new Array(this._gridWidth);//creating the multidem array in javascript <3
+    for (let i = 0; i < this._board.length; i++) {
+        this._board[i] = new Array(this._gridHeight);
+    }
+    for (let i = 0; i < this._gridWidth; i++) {
+        //////board set up
+        //assigns whole _board array with empty
+        for (let j = 0; j < this._gridHeight; j++) {
+            this._board[i][j] = "empty";
+        }
+    }
+    for (let i = 0; i < this._gridHeight; i++) {
+        //////board set up
+        //assigns boarder portions of _board array
+        this._board[0][i] = "boarder";
+        this._board[this._gridWidth-1][i] = "boarder";
+        if (i < this._gridWidth) {
+            this._board[i][0] = "boarder";
+            this._board[i][this._gridHeight-1] = "boarder";
+        }
+    }
+    for (let i = 0; i < this._gridHeight; i++) {
+        this._board[5][i] = "block";
+    }
+
+    this.update = function () {
+        if (this.isEmpty === true) {
+            this.placePiece();
+        }
+    }
+    this.placePiece = function () {
+
+    }
+    this.draw = function () {
+
+
+
+        for (let i = 0; i < this._gridWidth; i++) {
+            for (let j = 0; j < this._gridHeight; j++) {
+                //we need to set the specific color depending on what is in the array
+
+                //so this is where we pick different colors
+                c.beginPath();
+                c.fillStyle = 'red';
+         
+                if (this._board[i][j] === "boarder") {
+                    c.fillStyle = 'blue';
+                }
+                if (this._board[i][j] === "empty") {
+                    c.fillStyle = 'black';
+                }
+                if (this._board[i][j] === "block") {
+                    c.fillStyle = 'green';
+                }
+
+ 
+   
+                c.fillRect(i * this._cellSize, j * this._cellSize, this._cellSize, this._cellSize);
+                c.stroke();
+            }
+        }
+
+
+
+
+    }
+
+}
+
+function Piece() {
+
+}
+
+
+
 var mouse = {
     x: undefined, y: undefined
 }
@@ -72,113 +150,15 @@ window.addEventListener('mousemove', function (event) {
 });
 
 
-var distance = function (x1, y1, x2, y2) {
-
-    let dx = x2 - x1;
-    let dy = y2 - y1;
-    dx = dx * dx;
-    dy = dy * dy;
-    return Math.sqrt(dx + dy);
-}
-
-//Circle constructor
-function Circle(x, y, dx, dy, radius, r, g, b) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.a = 1;
-    this.ograd = radius;
-    this.radius = radius;
-    this.draw = function () {
-
-        c.beginPath();
-
-        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        c.strokeStyle = `rgba(${this.r},${this.g},${this.b},1)`;
-        c.fill();
-        c.stroke();
-    }
-    this.update = function () {
-        //resize code
-
-        if (distance(this.x, this.y, mouse.x, mouse.y) < this.radius + 40) {
-            this.radius = this.radius + 5;
-        }
-        else if (this.ograd < this.radius + 40) {
-            this.radius = this.radius - 30;
-        }
 
 
-        if ((this.radius < 0)||(this.radius >1000)) {//reseting big ass bounds
-            this.radius = this.ograd;
-        }
-        //resize code
-
-        this.x = this.x + this.dx;
-        this.y = this.y + this.dy;
-
-        if (this.x >= canvas.width - this.radius) {
-            this.dx = -this.dx;
-        }
-        else if (this.x - this.radius < 0) {
-            this.dx = -this.dx;
-        }
-
-        if (this.y >= canvas.height - this.radius) {
-            this.dy = -this.dy;
-        }
-        else if (this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
-
-
-
-
-
-
-
-    }
-
-}
-
-//array of circles
-var circles = [];
-for (var i = 0; i < 600; i++) {
-
-    let radius = 60 + 45*Math.random();
-
-    //from 30 to innerwidth - 30
-    let x = radius + (innerWidth - 3*radius) * Math.random();
-    let y = radius + (innerHeight - 3*radius) * Math.random();
-
-
-    let dx = 4 * (Math.random() - 0.5);
-    let dy = 4 * (Math.random() - 0.5);
-    let r = Math.floor(256 * Math.random());
-    let g = Math.floor(256 * Math.random());
-    let b = Math.floor(256 * Math.random());
-
-
-    var circle = new Circle(x, y, dx, dy, radius, r, g, b);
-
-    circles.push(circle);
-}
-
-
+var board = new Board();
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].draw();
-        circles[i].update();
-    }
-
+    board.draw();
 }
 
 animate();

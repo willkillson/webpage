9c596a880a80e9120341c384aa61b7fbe06e30b9
willkillson <willkillson@gmail.com>
2018-04-02 18:29:21 -0700

    Completed piece generation.

--- mywebpage/bin/Debug/AppX/js/main.js
+++ mywebpage/bin/Debug/AppX/js/main.js
@@ -46,24 +46,20 @@ var c = canvas.getContext('2d');
 //    c.arc(x, y, 30, 0, 2 * Math.PI, false);
 //    c.stroke();
 //}
-
+const GRIDHEIGHT = 25;
+const GRIDWIDTH = 11;
 function Board() {
- //////////////////////////////////////////rectangle
-//c.fillStyle = 'rgba(255,0,0,1)';
-//var GridSize = 40;
-//var fillamount = 1.0;
-//for (var i = 0; i < 8; i++) {
-//    for (var j = 0; j < 8; j++) {
-//        c.fillStyle = `rgba(255,0,0,${fillamount})`;
-//        c.fillRect(GridSize * i + i, GridSize * j + j, GridSize, GridSize);
-//    }
-//    fillamount = (fillamount - 2 * (1 / 16));
-//}
+
     this._gridHeight = 25;
-    this._gridWidth = 11;
+    this._gridWidth = 12;
     this._cellSize = 30;
-    this._cellPadding = 5;
+    this._cellPadding = 1;
     this.isEmpty = true;//this will invoke a place piece function
+    this.currentPiece = undefined;
+
+
+
+    /////////////////MAIN BOARD
 
     this._board = new Array(this._gridWidth);//creating the multidem array in javascript <3
     for (let i = 0; i < this._board.length; i++) {
@@ -76,6 +72,13 @@ function Board() {
             this._board[i][j] = "empty";
         }
     }
+
+
+
+
+
+
+
     for (let i = 0; i < this._gridHeight; i++) {
         //////board set up
         //assigns boarder portions of _board array
@@ -86,16 +89,23 @@ function Board() {
             this._board[i][this._gridHeight-1] = "boarder";
         }
     }
-    for (let i = 0; i < this._gridHeight; i++) {
-        this._board[5][i] = "block";
-    }
+
 
     this.update = function () {
-        if (this.isEmpty === true) {
-            this.placePiece();
+        if (this.currentPiece === undefined) {
+            this.currentPiece = new Piece();
+            this.currentPiece.generateNewPiece();
+
         }
-    }
-    this.placePiece = function () {
+
+
+        //Attempt to move the piece
+        //Verify legal move
+        //Verify piece is not being set
+        //Place the piece into the boardcheck
+        //this._boardCheck[this.currentPiece._xPosition][this.currentPiece._yPosition] = "block";
+
+
 
     }
     this.draw = function () {
@@ -120,9 +130,28 @@ function Board() {
                     c.fillStyle = 'green';
                 }
 
- 
-   
-                c.fillRect(i * this._cellSize, j * this._cellSize, this._cellSize, this._cellSize);
+                if (this.currentPiece._boardCheck[i][j] === "orange") {
+                    c.fillStyle = 'orange';
+                }
+                if (this.currentPiece._boardCheck[i][j] === "teal") {
+                    c.fillStyle = 'teal';
+                }
+                if (this.currentPiece._boardCheck[i][j] === "red") {
+                    c.fillStyle = 'red';
+                }
+                if (this.currentPiece._boardCheck[i][j] === "blue") {
+                    c.fillStyle = 'blue';
+                }
+                if (this.currentPiece._boardCheck[i][j] === "purple") {
+                    c.fillStyle = 'purple';
+                }
+                if (this.currentPiece._boardCheck[i][j] === "green") {
+                    c.fillStyle = 'green';
+                }
+                if (this.currentPiece._boardCheck[i][j] === "yellow") {
+                    c.fillStyle = 'yellow';
+                }
+                c.fillRect(i * this._cellSize + this._cellPadding * i, j * this._cellSize + j * this._cellPadding, this._cellSize, this._cellSize);
                 c.stroke();
             }
         }
@@ -135,7 +164,86 @@ function Board() {
 }
 
 function Piece() {
+    this._pieceType = undefined;//0===I , 1===T , 2===L , 3===J , 4===S , 5===Z , 6===BLOCK
+    this._xPosition = undefined;// in relation to the main board
+    this._yPosition = undefined;// in relation to the main board
+    this._rotation = undefined;
+    this._gridHeight = 25;
+    this._gridWidth = 12;
 
+    this._boardCheck = new Array(this._gridWidth);//creating the multidem array in javascript <3
+    for (let i = 0; i < this._boardCheck.length; i++) {
+        this._boardCheck[i] = new Array(this._gridHeight);
+    }
+
+
+
+    this.generateNewPiece = function () {
+        let num = Math.floor(7 * Math.random());
+
+        for (let i = 0; i < this._gridWidth; i++) {
+            //////board set up
+            //assigns whole _board array with empty
+            for (let j = 0; j < this._gridHeight; j++) {
+                this._boardCheck[i][j] = "empty";
+            }
+        }
+
+
+
+        //0===I , 1===T , 2===L , 3===J , 4===S , 5===Z , 6===O
+        this._xPosition = 5;// 
+        this._yPosition = 3;// 
+        switch (num) {
+            case 0://I piece
+                this._boardCheck[this._xPosition][this._yPosition] = "red";
+                this._boardCheck[this._xPosition + 1][this._yPosition] = "red";
+                this._boardCheck[this._xPosition + 2][this._yPosition] = "red";
+                this._boardCheck[this._xPosition - 1][this._yPosition] = "red";
+                this._rotation = 0;//2 rotations
+                break;
+            case 1://T
+                this._boardCheck[this._xPosition][this._yPosition] = "teal";
+                this._boardCheck[this._xPosition - 1][this._yPosition] = "teal";
+                this._boardCheck[this._xPosition  + 1][this._yPosition ] = "teal";
+                this._boardCheck[this._xPosition][this._yPosition+1] = "teal";
+                break;
+            case 2://L
+                this._boardCheck[this._xPosition][this._yPosition] = "orange";
+                this._boardCheck[this._xPosition+1][this._yPosition] = "orange";
+                this._boardCheck[this._xPosition-1][this._yPosition] = "orange";
+                this._boardCheck[this._xPosition-1][this._yPosition+1] = "orange";
+                break;
+            case 3://J
+                this._boardCheck[this._xPosition][this._yPosition] = "blue";
+                this._boardCheck[this._xPosition-1][this._yPosition] = "blue";
+                this._boardCheck[this._xPosition+1][this._yPosition] = "blue";
+                this._boardCheck[this._xPosition+1][this._yPosition+1] = "blue";
+                break;
+            case 4://S
+                this._boardCheck[this._xPosition][this._yPosition] = "purple";
+                this._boardCheck[this._xPosition + 1][this._yPosition] = "purple";
+                this._boardCheck[this._xPosition][this._yPosition+1] = "purple";
+                this._boardCheck[this._xPosition - 1][this._yPosition + 1] = "purple";
+                break;
+            case 5://Z
+                this._boardCheck[this._xPosition][this._yPosition] = "green";
+                this._boardCheck[this._xPosition-1][this._yPosition] = "green";
+                this._boardCheck[this._xPosition][this._yPosition+1] = "green";
+                this._boardCheck[this._xPosition+1][this._yPosition+1] = "green";
+                break;
+            case 6://O
+                this._boardCheck[this._xPosition][this._yPosition] = "yellow";
+                this._boardCheck[this._xPosition+1][this._yPosition] = "yellow";
+                this._boardCheck[this._xPosition+1][this._yPosition+1] = "yellow";
+                this._boardCheck[this._xPosition][this._yPosition+1] = "yellow";
+                break;
+        }
+
+
+
+
+    }
 }
 
 
@@ -158,6 +266,7 @@ function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
 
+    board.update();
     board.draw();
 }
 
